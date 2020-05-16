45a479b89a030003f8b4ddcae0662d43f8f08016
netanel <orez132@gmail.com>
2020-05-17 01:14:00 +0300

    Add landing path

--- src/spacesship/SpaceshipLanding.java
+++ src/spacesship/SpaceshipLanding.java
@@ -1,15 +1,22 @@
 package spacesship;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("Duplicates")
 public class SpaceshipLanding implements SpaceshipActions {
 
     private Spaceship spaceship;
     private int landing_time;
     private double power = 0;
+    
+    private int num_of_destination_points = 9;
+    private ArrayList<Point> destination_points;
+    private Point current_destination_point;
 
     public SpaceshipLanding(Spaceship spaceship) {
         this.landing_time = 0;
         this.spaceship = spaceship;
+        defineLandingPath();
     }
 
     @Override
@@ -58,6 +65,31 @@ public class SpaceshipLanding implements SpaceshipActions {
         landing_time++;
     }
 
+    //Define the desired landing path, which the spacecraft will land on
+    private void defineLandingPath() {
+    	this.destination_points = new ArrayList<Point>();
+    	double total_alt = this.spaceship.getStartAltitudeFromMoon();
+    	double total_dis = this.spaceship.getStartDistanceFromDestination();
+    	double dis_from_ver_landing = 3000;          //arbitrary
+    	double alt_from_ver_landing = 2000;          //arbitrary
+    	double dis = total_dis - dis_from_ver_landing;
+    	double alt = total_alt - alt_from_ver_landing;
+    	int num_points = this.num_of_destination_points;
+    	double dis_between_points = dis/num_points;
+    	double alt_between_points = alt/num_points;
+    	
+    	this.destination_points.add(new Point(0, total_alt));
+    	
+    	double x=0, y=total_alt;
+    	for(int i=0 ; i<num_points ; i++) {
+    		x += dis_between_points;
+    		y -= alt_between_points;
+    		this.destination_points.add(new Point(x,y));
+    	}
+    	
+    	this.current_destination_point = this.destination_points.get(1);
+    }
+    
     private void fullPowerLanding() {
         double acc = Formulas.calcAccelerate(spaceship.getActualWeight(), true, 8);
         updateSpeed(acc);
